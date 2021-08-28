7b73bc7447b0b4223198dbf9cfd8b519dc9cf9f7
mtelias2 <mtelias2@illinois.edu>
2021-08-28 17:05:24 -0500

    just trial push

--- Practice projects/main.py
+++ Practice projects/main.py
@@ -1,1 +1,5 @@
-#This is my first project from the list: https://www.analyticsvidhya.com/blog/2018/05/24-ultimate-data-science-projects-to-boost-your-knowledge-and-skills/
+# This is my first project from the list:
+# https://www.analyticsvidhya.com/blog/2018/05/24-ultimate-data-science-projects-to-boost-your-knowledge-and-skills/
+# The first project with pylint included.
+# Will go back and fix all pylint issues
+
