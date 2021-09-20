b06b5eec649e5e7ccdedd67b41f7a72f7eea01a7
mtelias2 <moutazelias@hotmail.com>
2021-09-19 19:32:05 -0500

    rearranged files

--- Practice projects/main.py
+++
@@ -1,5 +0,0 @@
-# This is my first project from the list:
-# https://www.analyticsvidhya.com/blog/2018/05/24-ultimate-data-science-projects-to-boost-your-knowledge-and-skills/
-# The first project with pylint included.
-# Will go back and fix all pylint issues
-
